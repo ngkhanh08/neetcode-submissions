99ace0368841f809ea2f766e31896e0db52bfc6b
ngkhanh08 <ngkhanh0812@gmail.com>
2026-09-25 00:51:21 -0400

    Add: rotting-fruit - submission-0

---
+++ Data Structures & Algorithms/rotting-fruit/submission-0.java
@@ -0,0 +1,73 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        Queue<int[]> queue = new LinkedList<>();
+
+        int fresh = 0;
+
+        // Put all rotten oranges into queue
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+
+                if (grid[r][c] == 2) {
+                    queue.offer(new int[]{r, c});
+                }
+
+                if (grid[r][c] == 1) {
+                    fresh++;
+                }
+            }
+        }
+
+        int time = 0;
+
+        int[][] directions = {
+            {1, 0},
+            {-1, 0},
+            {0, 1},
+            {0, -1}
+        };
+
+        while (!queue.isEmpty() && fresh > 0) {
+
+            int size = queue.size();
+
+            // One minute
+            for (int i = 0; i < size; i++) {
+
+                int[] curr = queue.poll();
+
+                int r = curr[0];
+                int c = curr[1];
+
+                for (int[] dir : directions) {
+
+                    int nr = r + dir[0];
+                    int nc = c + dir[1];
+
+                    if (nr < 0 || nr >= rows ||
+                        nc < 0 || nc >= cols) {
+                        continue;
+                    }
+
+                    // Only fresh oranges can rot
+                    if (grid[nr][nc] != 1) {
+                        continue;
+                    }
+
+                    grid[nr][nc] = 2;
+                    fresh--;
+
+                    queue.offer(new int[]{nr, nc});
+                }
+            }
+
+            time++;
+        }
+
+        return fresh == 0 ? time : -1;
+    }
+}
